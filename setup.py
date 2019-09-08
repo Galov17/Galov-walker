6ce3be73506be307d51f0e887a2dbe3f2f4cab56
Galov17 <55056007+Galov17@users.noreply.github.com>
2019-09-08 17:04:58 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+
+setup(name='galov-walker',
+
+      version='0.0.1',
+
+      description='OpenAI Gym environment for Galov-walker',
+
+      author='Antonio Galov',
+
+      install_requires=['gym', 'numpy', 'Box2D','evostra']
+
+)
